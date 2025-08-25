de1861b2ca6312e3e3e2c1f26058cf8a48a59d6e
Jesse <itsredx000@gmail.com>
2025-08-25 15:10:36 +0330

    License change and cleanup
    remove build files from git

--- cmake-build-debug/CMakeFiles/ProjectLibrary.dir/Source/Settings.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ProjectLibrary.dir/Source/Settings.cpp.obj: \
- E:\C++\ Projects\JGraphicEngine\JGraphicEngine\Source\Settings.cpp \
- E:\C++\ Projects\JGraphicEngine\JGraphicEngine\Source\Settings.h
