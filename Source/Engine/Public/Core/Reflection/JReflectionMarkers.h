533d7229b29c281511a5f564d14e4b2254aae6ab
Jesse <itsredx000@gmail.com>
2026-01-26 00:25:04 +0330

    Implement JReflectGen V0.1

---
+++ Source/Engine/Public/Core/Reflection/JReflectionMarkers.h
@@ -0,0 +1,11 @@
+//  Copyright 2025-2026 JesseTheCatLover. All Rights Reserved.
+
+#pragma once
+
+// Markers for the code generator (no-op for C++)
+#define JCLASS(...)
+#define JSTRUCT(...)
+#define JENUM(...)
+#define JFUNCTION(...)
+#define JPROPERTY(...)
+#define JGENERATED_BODY()
