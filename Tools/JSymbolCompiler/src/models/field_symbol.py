5352d263f91168fedf413efd4742f4f073b08ae9
Jesse <itsredx000@gmail.com>
2026-06-09 18:24:58 +0330

    Move JSymbolCompiler Into Its Own Repo

--- Tools/JSymbolCompiler/src/models/field_symbol.py
+++
@@ -1,9 +0,0 @@
-#  Copyright 2025-2026 JesseTheCatLover. All Rights Reserved.
-
-from dataclasses import dataclass
-from .base import Symbol
-
-@dataclass
-class FieldSymbol(Symbol):
-    type: str = ""
-    owner: str = ""
